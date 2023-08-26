e9d8c5124debc6259ae17ae4f37b40d1445ff775
edmond-chow <45330596+edmond-chow@users.noreply.github.com>
2023-08-26 17:05:58 +0800

    Add files via upload

--- C++/Sedenion.rar/Sedenion/Complex Testing Console/Base.h
+++ C++/Sedenion.rar/Sedenion/Complex Testing Console/Base.h
@@ -96,7 +96,6 @@ namespace ComplexTestingConsole
 	public:
 		static std::wstring GetTitle();
 		static std::wstring GetStartupLine();
-		static std::wstring GetSedenTitle();
 		static bool IsSwitchTo(const std::wstring& Str);
 		///
 		/// Main Thread
