f7fd6da93df2610d8bd1262ead61a1bc9aea8b29
edmond-chow <45330596+edmond-chow@users.noreply.github.com>
2023-09-03 12:39:22 +0800

    Add files via upload

--- C#/Complex.rar/Complex/Complex Testing Console/Base Extension.cs
+++ C#/Complex.rar/Complex/Complex Testing Console/Base Extension.cs
@@ -3,30 +3,10 @@
 /*   Extension   */
 /*               */
 /* ============= */
-using System;
-namespace CmplxConExt
+internal static class CmplxConExt
 {
-    internal class Original
+    private static void Main()
     {
-        private static readonly ConsoleColor ForegroundColor;
-        private static readonly ConsoleColor BackgroundColor;
-        private static readonly string Title;
-        static Original()
-        {
-            ForegroundColor = Console.ForegroundColor;
-            BackgroundColor = Console.BackgroundColor;
-            Title = Console.Title;
-        }
-        private static void Release()
-        {
-            Console.ForegroundColor = ForegroundColor;
-            Console.BackgroundColor = BackgroundColor;
-            Console.Title = Title;
-        }
-        private static void Main()
-        {
-            ComplexTestingConsole.Base.Main();
-            Release();
-        }
+        ComplexTestingConsole.Base.Main();
     }
 }
--- C#/Complex.rar/Complex/Sedenion Testing Console/Base Extension.cs
+++ C#/Complex.rar/Complex/Sedenion Testing Console/Base Extension.cs
@@ -3,30 +3,10 @@
 /*   Extension   */
 /*               */
 /* ============= */
-using System;
-namespace SedenConExt
+internal static class SedenConExt
 {
-    internal class Original
+    private static void Main()
     {
-        private static readonly ConsoleColor ForegroundColor;
-        private static readonly ConsoleColor BackgroundColor;
-        private static readonly string Title;
-        static Original()
-        {
-            ForegroundColor = Console.ForegroundColor;
-            BackgroundColor = Console.BackgroundColor;
-            Title = Console.Title;
-        }
-        private static void Release()
-        {
-            Console.ForegroundColor = ForegroundColor;
-            Console.BackgroundColor = BackgroundColor;
-            Console.Title = Title;
-        }
-        private static void Main()
-        {
-            SedenionTestingConsole.Base.Main();
-            Release();
-        }
+        SedenionTestingConsole.Base.Main();
     }
 }
